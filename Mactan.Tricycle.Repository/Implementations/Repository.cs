cabc48c5fbe0de8d4882289dc77c4edb012d512d
PMAX-Ric-Ybanez <ekopay.b2b@gmail.com>
2019-03-27 14:17:35 +0800

    add repositories

---
+++ Mactan.Tricycle.Repository/Implementations/Repository.cs
@@ -0,0 +1,77 @@
+namespace Mactan.Tricycle.Repository.Implementations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Mactan.Tricycle.DAL.Interfaces;
+    using Mactan.Tricycle.Repository.Interfaces;
+
+    public class Repository<TContext> : ReadOnlyRepository<TContext>, IRepository
+    where TContext : DbContext
+    {
+        public Repository(TContext context)
+            : base(context)
+        {
+        }
+
+        public virtual void Create<TEntity>(TEntity entity, string createdBy = null)
+            where TEntity : class, IEntity
+        {
+            entity.CreatedBy = createdBy;
+            context.Set<TEntity>().Add(entity);
+        }
+
+        public virtual void Update<TEntity>(TEntity entity, string modifiedBy = null)
+            where TEntity : class, IEntity
+        {
+            entity.UpdatedBy = modifiedBy;
+            context.Set<TEntity>().Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete<TEntity>(object id)
+            where TEntity : class, IEntity
+        {
+            TEntity entity = context.Set<TEntity>().Find(id);
+            Delete(entity);
+        }
+
+        public virtual void Delete<TEntity>(TEntity entity)
+            where TEntity : class, IEntity
+        {
+            var dbSet = context.Set<TEntity>();
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+
+        public virtual void Save()
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public virtual Task SaveAsync()
+        {
+            try
+            {
+                return context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Mactan.Tricycle.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+namespace Mactan.Tricycle.Repository.Interfaces
+{
+    using System.Threading.Tasks;
+    using Mactan.Tricycle.DAL.Interfaces;
+    public interface IRepository : IReadOnlyRepository
+    {
+        void Create<TEntity>(TEntity entity, string createdBy = null)
+            where TEntity : class, IEntity;
+
+        void Update<TEntity>(TEntity entity, string modifiedBy = null)
+            where TEntity : class, IEntity;
+
+        void Delete<TEntity>(object id)
+            where TEntity : class, IEntity;
+
+        void Delete<TEntity>(TEntity entity)
+            where TEntity : class, IEntity;
+
+        void Save();
+
+        Task SaveAsync();
+    }
+}
